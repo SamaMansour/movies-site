483ff8eb4784f18b880fd64a91dd9234c22f27af
SamaMansour <sma302000@gmail.com>
2023-03-02 14:24:39 +0300

    Remove API_KEY from everywhere

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,9 @@ export const BASE_API = axios.create({ baseURL: 'https://api.themoviedb.org/3/' 
 const BASE_AXIOS = axios.create({baseURL: 'https://api.themoviedb.org/3%22%7D'});
 export const API_KEY= process.env.REACT_APP_API_KEY;
 export const Popular_URL =
-  'https://api.themoviedb.org/3/movie/popular?api_key=14ccdb96456935bbb41591e99697d262&language=en-US';
+  `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`;
 export const Discover_URL =
-  'https://api.themoviedb.org/3/discover/movie?api_key=14ccdb96456935bbb41591e99697d262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_watch_monetization_types=flatrate';
+  `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_watch_monetization_types=flatrate`;
 
 
 export const img_url='http://image.tmdb.org/t/p/w500';
